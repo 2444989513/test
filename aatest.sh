3261ba96e1718c038a1722226219af9891135f66
2444989513 <56295510+2444989513@users.noreply.github.com>
2021-09-08 03:35:16 +0800

    Update aatest.sh

--- aatest.sh
+++ aatest.sh
@@ -24,8 +24,8 @@ nginx_systemd_file="/etc/systemd/system/nginx.service"
 #openssl_version="3.0.0-beta2"
 #--with-cc-opt="-Wno-error"   \
 
-nginx_version="1.21.2"
-openssl_version="1.1.1l"
+nginx_version="1.21.3"
+openssl_version="3.0.0"
 pcre_version="8.45"
 
 
