1a334ce8db2cfe69ba9a0019d34411d07468d6eb
2444989513 <56295510+2444989513@users.noreply.github.com>
2025-04-09 18:40:58 +0800

    Update aatest.sh

--- aatest.sh
+++ aatest.sh
@@ -21,11 +21,11 @@ nginx_dir="/etc/nginx"
 nginx_openssl_src="/usr/local/src"
 nginx_systemd_file="/etc/systemd/system/nginx.service"
 
-openssl_version="3.5.0-alpha1"
+#openssl_version="3.5.0-alpha1"
 #--with-cc-opt="-Wno-error"   \
 
 nginx_version="1.27.4"
-#openssl_version="3.4.1"
+openssl_version="3.5.0"
 pcre2_version="10.45"
 
 #libunwind_version="1.5.0"
