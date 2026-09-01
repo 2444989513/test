5a062620611a069a24df0de75d2ab668efd5c6d8
2444989513 <56295510+2444989513@users.noreply.github.com>
2026-09-02 00:47:52 +0800

    Update pcre2 version to 10.48

--- aatest.sh
+++ aatest.sh
@@ -26,7 +26,7 @@ nginx_systemd_file="/etc/systemd/system/nginx.service"
 
 nginx_version="1.31.4"
 openssl_version="4.0.2"
-pcre2_version="10.48-RC1"
+pcre2_version="10.48"
 
 #libunwind_version="1.5.0"
 #google_perftools_version="2.9.1"
